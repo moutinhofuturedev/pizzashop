60cd8c03b29ee4bacee31e9cb7b979c7de23a68a
Paulo Moutinho <paulo_vicali@icloud.com>
2024-01-26 17:04:39 -0300

    feature(master): inicialização do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './global.css'
+import { Button } from "@/components/ui/button"
+
+
+export const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
+  )
+}
+
+export default App
