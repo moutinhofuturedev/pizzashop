5bb2adb70fdb00044d583bef35254ff3a6081b5f
Paulo Moutinho <paulo_vicali@icloud.com>
2024-02-15 13:48:56 -0300

    feature(main): Página Login

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,3 +1,13 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1>Dashbooard</h1>
+      <Button variant="outline">Enviar</Button>
+    </>
+  )
 }
