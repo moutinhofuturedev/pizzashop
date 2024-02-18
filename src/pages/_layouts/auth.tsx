f067a9171f0c54d21a99c17b703a8d416ed24e6d
Paulo Moutinho <paulo_vicali@icloud.com>
2024-02-18 18:25:53 -0300

    feature(main): cria componente header

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,7 +2,7 @@ import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 export const AuthLayout = () => {
   return (
-    <div className="min-h-screen grid grid-cols-2">
+    <div className="min-h-screen grid grid-cols-2 antialiased">
       <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
