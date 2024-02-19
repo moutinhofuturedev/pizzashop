1fe539f1bcbd4bbb97315edbc75684e9117bf41e
Paulo Moutinho <paulo_vicali@icloud.com>
2024-02-19 20:29:45 -0300

    refactor(main): remove botão

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,13 +1,10 @@
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '@/components/ui/button'
-
 export const Dashboard = () => {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashbooard</h1>
-      <Button variant="outline">Enviar</Button>
+      <h1>Dashboard</h1>
     </>
   )
 }
