f22376dee307c131e6abab1fac6077d53979a99e
Paulo Moutinho <paulo_vicali@icloud.com>
2024-02-26 10:18:36 -0300

    refactor(main): muda importações

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './pages/_layouts/app'
-import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/home/dashboard'
-import { Orders } from './pages/app/orders/orders'
-import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sign-up'
+import { AppLayout } from '@/pages/_layouts/app'
+import { AuthLayout } from '@/pages/_layouts/auth'
+import { Dashboard } from '@/pages/app/home/dashboard'
+import { Orders } from '@/pages/app/orders/orders'
+import { SignIn } from '@/pages/auth/sign-in'
+import { SignUp } from '@/pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
