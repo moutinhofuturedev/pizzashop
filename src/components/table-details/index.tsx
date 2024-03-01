c3a998141b8e9ead29341081dc9758e330e4b480
Paulo Moutinho Vitor <paulo.vitor@viavarejo.com>
2024-03-01 18:56:36 -0300

    feature(main): cria componente de detalhes de pedidos

---
+++ src/components/table-details/index.tsx
@@ -0,0 +1,103 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export const TableDetails = () => {
+  const orderId = crypto.randomUUID()
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedido: {orderId}</DialogTitle>
+        <DialogDescription>Detalhes do pedido</DialogDescription>
+      </DialogHeader>
+
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400" />
+                  <span className="font-medium text-muted-foreground">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex justify-end">
+                Paulo Moutinho Vitor
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex justify-end">(11) 965353340</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                paulo_vicali@icloud.com
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realizado há
+              </TableCell>
+              <TableCell className="flex justify-end">3 minutos</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Qtd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza Peperoni Família</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">R$ 70,00</TableCell>
+              <TableCell className="text-right">R$ 70,00</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>Pizza Mussarela</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">R$ 50,00</TableCell>
+              <TableCell className="text-right">R$ 50,00</TableCell>
+            </TableRow>
+          </TableBody>
+
+          <TableFooter>
+            <TableCell colSpan={3}>Total do pedido</TableCell>
+            <TableCell className="text-right font-medium">R$ 120,00</TableCell>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
